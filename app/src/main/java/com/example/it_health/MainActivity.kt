a6bbf86b5a7b08f819b95a8e08e893a44ef71c32
13Rvladislav <rozbash@sfedu.com>
2024-03-30 20:25:48 +0300

    Сохранение авторизации

--- app/src/main/java/com/example/it_health/MainActivity.kt
+++ app/src/main/java/com/example/it_health/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.it_health
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -7,8 +8,11 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,18 +21,27 @@ class MainActivity : AppCompatActivity() {
             //создание переменных кнопок
         val RegBtn = findViewById<Button>(R.id.registration)
         val AuthBtn = findViewById<Button>(R.id.authorization)
+        auth = FirebaseAuth.getInstance()
+
+        // Проверяем, авторизован ли пользователь
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // Пользователь уже авторизован, переходим на главный экран
+            startActivity(Intent(this, ActivityMainMenu::class.java))
+        } else {
+
 
             //кнопка регистрации
-        RegBtn.setOnClickListener(){
-           val intent =Intent(this,RegActivity::class.java)
-            startActivity(intent)
-        }
+            RegBtn.setOnClickListener() {
+                val intent = Intent(this, RegActivity::class.java)
+                startActivity(intent)
+            }
             //кнопка авторизации
-        AuthBtn.setOnClickListener(){
-            val intent =Intent(this,AuthorActivity::class.java)
-            startActivity(intent)
+            AuthBtn.setOnClickListener() {
+                val intent = Intent(this, AuthorActivity::class.java)
+                startActivity(intent)
+            }
         }
-
 
 
 
